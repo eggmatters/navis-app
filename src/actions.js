188f16594f3a9928cd45904e92aa451ce3dd7ba8
Matthew Eggers <matthew.eggers@nike.com>
2019-01-08 20:49:46 -0800

    fetched movies uri

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,6 @@
-const apiKey = '057dfa32a18eed0f2dc23dc2e80ed8a0';
-//const urlBase = 'https://api.themoviedb.org/3/search/movie/';
 const urlBase = 'http://localhost:3001/'
 
 let url = null;
-let queryString = null;
 
 
 export const FETCH_MOVIES_BEGIN   = 'FETCH_MOVIES_BEGIN';
@@ -25,23 +22,11 @@ export const fetchMoviesFailure = error => ({
 });
 
 export const fetchMovies = searchString => {
-  queryString = '?page=1&include_adult=false&language=en-US&api_key=' + apiKey + '&query=' + searchString;
-  url = urlBase + queryString;
+  url = urlBase + '?searchString=' + searchString;
 
   return dispatch => {
     dispatch(fetchMoviesBegin());
-    return fetch(url)
-      // .then(response => {
-      //   console.log(response);
-      //   return response;
-      // })
-      // .then((data) => {
-      //   console.log(data);
-      //   //handleErrors(data);
-      //   //resolve(data ? JSON.parse(data) : {});
-      //   dispatch(fetchMoviesSuccess(JSON.parse(data)));
-      // })
-      //.catch((error) => dispatch(fetchMoviesFailure(error)));
+    return fetch(encodeURI(url))
       .then(handleErrors)
       .then(res => res.json())
       .then(json => {
--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,50 @@
-const http = require('http');
-const url = require('url');
-const Promise = require('promise');
+const http  = require('http');
+const https = require('https');
+const url   = require('url');
+const request = require('request-promise');
+
+const apiKey = '057dfa32a18eed0f2dc23dc2e80ed8a0';
+const urlBase = 'http://api.themoviedb.org/3/search/movie/';
+
+let requestUrl = null,
+    searchString = null;
 
 const server = http.createServer((req, response) => {
-    const parsedUrl = url.parse(req.url, true);
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname,
+        query = parsedUrl.query;
 
-    const path = parsedUrl.pathname, query = parsedUrl.query;
-    if (path == '/favicon.ico') {
-      return;
-    }
-    const method = req.method;
+  if (path == '/favicon.ico') {
+    return;
+  }
+  searchString = query.searchString;
+  requestUrl = urlBase + '?page=1&include_adult=false&language=en-US&api_key=' + apiKey + '&query=' + searchString;
 
-    let responseJSON = JSON.stringify(query);
+  console.log(`Request received on: ${path} + query: ${JSON.stringify(query)}`);
+  console.log('query: ', query);
 
-    console.log(`Request received on: ${path} + method: ${method} + query: ${responseJSON}`);
-    console.log('query: ', query);
-
+  fetchMovies(requestUrl).then( (responseJSON) => {
     response.writeHead(200, {
-      //'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept",
     });
+
     response.write(responseJSON);
     response.end();
   });
+});
 
+const fetchMovies = (url) => {
+  console.log('fetching:', url);
+  try {
+    return request(url).then( (response) => {
+      return response;
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
+}
 
-  server.listen(3001, () => console.log("Server running at port 3001"));
+server.listen(3001, () => console.log("Server running at port 3001"));
